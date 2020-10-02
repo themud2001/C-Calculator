cab90687767e117b66ca0eb518872585d733b539
superb-keshav <68380870+superb-keshav@users.noreply.github.com>
2020-10-02 14:35:15 +0530

    Update test.cpp
    Added a second program of c++ calculator which is very simple and easy to read.

--- test.cpp
+++ test.cpp
@@ -1,3 +1,5 @@
+//First program of calculator
+
 #include <iostream>
 #include <string>
 #include "Calculator.h"
@@ -14,4 +16,44 @@ int main() {
 	cout<<calc.evaluate()<<endl;
 
 	return 0;
+}
+
+//Second program of calculator
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, d;
+    char c;
+    cout << "enter operand:";
+    cin >> c;
+
+    if (c == '+')
+    {
+        cin >> a >> b;
+        d = a + b;
+        cout <<"a+b="<< d;
+    }
+    if (c == '-')
+    {
+        cin >> a >> b;
+        d = a - b;
+        cout <<"a-b="<< d;
+    }
+    if (c == '/')
+    {
+        cin >> a >> b;
+        d = a / b;
+        cout <<"a/b="<< d;
+    }
+    if (c == '*')
+    {
+        cin >> a >> b;
+        d = a * b;
+
+        cout <<"a*b="<< d;
+    }
+    return 0;
 }
